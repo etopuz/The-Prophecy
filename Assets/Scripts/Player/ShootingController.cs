9a49e3373e12e8a382fd7adbeae79188f35cfa25
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-06-28 03:34:58 +0300

    change: aim and fire with joystick instead of button

--- Assets/Scripts/Player/ShootingController.cs
+++ Assets/Scripts/Player/ShootingController.cs
@@ -1,20 +1,26 @@
-using System.Collections;
 using UnityEngine;
-using TheProphecy.Player;
+using TheProphecy.Projectiles;
 
 namespace TheProphecy.Player
 {
     public class ShootingController : MonoBehaviour
     {
-        public static ObjectPool _pool;
-        private MovementController _movementController;
 
+        public static ObjectPool _pool;
+        private Vector2 _direction = new Vector2(1, 0);
+
+        [Header("References")]
+        [SerializeField] private Joystick _aimJoystick;
         [SerializeField] private GameObject _bulletPrefab;
         [SerializeField] private GameObject _projectileContainer;
+        [SerializeField] private GameObject _gunHoldingPoint;
 
-        [SerializeField] private float _bulletSpeed = 25f;
-        
 
+        [Header("Bullet Stats")]
+        [SerializeField] private float _bulletSpeed = 15f;
+        private const float FIRE_COOLDOWN_TIME = 0.3f;
+        private float _fireCooldownTimer = 0f;
+        private bool _isFireOnCooldown = false;
         // private float _bulletSize = 1f;
         // private int _bulletCount = 1;
 
@@ -22,22 +28,51 @@ namespace TheProphecy.Player
         {
             _pool = new ObjectPool(_bulletPrefab, _projectileContainer);
             _pool.FillThePool(30);
-            _movementController = GetComponent<MovementController>();
+        }
+
+        private void Update()
+        {
+            CalculateFireCooldown();
+            ShootProjectile();
         }
 
         public void ShootProjectile()
         {
-            
-            GameObject bullet = _pool.GetFromPool();
-            Rigidbody2D bullet_Rb = bullet.GetComponent<Rigidbody2D>();
+            if (_aimJoystick.Horizontal != 0 || _aimJoystick.Vertical != 0)
+            {
+                _direction = _aimJoystick.Direction;
+                float directionAngle = Vector2.SignedAngle(new Vector2(1, 0), _direction);
+                _gunHoldingPoint.transform.rotation = Quaternion.Euler(0, 0, directionAngle);
 
-            float directionAngle = Vector2.SignedAngle(new Vector2(1, 0), _movementController.Direction);
-            bullet.transform.rotation = Quaternion.Euler(0, 0, directionAngle);
-            bullet.transform.position = transform.position;
+                if (!_isFireOnCooldown)
+                {
+                    GameObject bullet = _pool.GetFromPool();
+                    Bullet bulletScript = bullet.GetComponent<Bullet>();
 
-            bullet_Rb.velocity = _movementController.Direction * _bulletSpeed;
+                    bulletScript.FireAndMove(_gunHoldingPoint.transform.position, _direction, directionAngle, _bulletSpeed);
+
+                    _isFireOnCooldown = true;
+                }
+            }
+
         }
 
+        private void CalculateFireCooldown()
+        {
+            if (_isFireOnCooldown)
+            {
+                if (_fireCooldownTimer > FIRE_COOLDOWN_TIME)
+                {
+                    _fireCooldownTimer = 0f;
+                    _isFireOnCooldown = false;
+                }
+
+                else
+                {
+                    _fireCooldownTimer += Time.deltaTime;
+                }
+            }
+        }
     }
 }
 
