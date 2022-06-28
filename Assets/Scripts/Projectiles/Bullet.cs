e957b5d8e5ef201c8b4b7063dd5f2a7b31996e98
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-06-28 03:35:46 +0300

    add: "FireAndMove" method - called by shooting controller

--- Assets/Scripts/Projectiles/Bullet.cs
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -9,6 +9,19 @@ namespace TheProphecy.Projectiles
     public class Bullet : MonoBehaviour
     {
         private int _damage = 1;
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void FireAndMove(Vector3 position, Vector3 direction, float angleZ, float bulletSpeed)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, angleZ);
+            transform.position = position;
+            _rigidbody.velocity = direction * bulletSpeed;
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
