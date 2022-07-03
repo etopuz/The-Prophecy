96788a24295d9a056677c4239cab31092d9bb0d7
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-03 20:56:05 +0300

    add: wall generation

--- Assets/Scripts/Grid/DungeonGeneration/TilemapVisualizer.cs
+++ Assets/Scripts/Grid/DungeonGeneration/TilemapVisualizer.cs
@@ -10,7 +10,7 @@ namespace TheProphecy.Grid.DungeonGeneration
     {
         [SerializeField] private Tilemap floorTilemap, wallTilemap;
         [SerializeField] private TileBase floorTile, wallTop, 
-            wallSideRight, wallSiderLeft, wallBottom, wallFull,
+            wallSideRight, wallSideLeft, wallBottom, wallFull,
             wallInnerCornerDownLeft, wallInnerCornerDownRight,
             wallDiagonalCornerDownRight, wallDiagonalCornerDownLeft, wallDiagonalCornerUpRight, wallDiagonalCornerUpLeft;
 
@@ -30,12 +30,88 @@ namespace TheProphecy.Grid.DungeonGeneration
         public void PaintSingleBasicWall(Vector2Int position, string binaryType)
         {
             int typeAsInt = Convert.ToInt32(binaryType, 2);
-            PaintSingleTile(wallTilemap, wallTop, position);
+            TileBase tile = null;
+
+            if (WallTypesHelper.wallTop.Contains(typeAsInt))
+            {
+                tile = wallTop;
+            }
+
+            else if (WallTypesHelper.wallSideRight.Contains(typeAsInt))
+            {
+                tile = wallSideRight;
+            }
+
+            else if (WallTypesHelper.wallSideLeft.Contains(typeAsInt))
+            {
+                tile = wallSideLeft;
+            }
+
+            else if (WallTypesHelper.wallBottom.Contains(typeAsInt))
+            {
+                tile = wallBottom;
+            }
+
+            else if (WallTypesHelper.wallFull.Contains(typeAsInt))
+            {
+                tile = wallFull;
+            }
+
+            if (tile != null)
+            {
+                PaintSingleTile(wallTilemap, tile, position);
+            }
+            
+        }
+
+        public void PaintSingleCornerWall(Vector2Int position, string binaryType)
+        {
+            int typeAsInt = Convert.ToInt32(binaryType, 2);
+            TileBase tile = null;
+
+            if (WallTypesHelper.wallInnerCornerDownLeft.Contains(typeAsInt))
+            {
+                tile = wallInnerCornerDownLeft;
+            }
+            else if (WallTypesHelper.wallInnerCornerDownRight.Contains(typeAsInt))
+            {
+                tile = wallInnerCornerDownRight;
+            }
+            else if (WallTypesHelper.wallDiagonalCornerDownLeft.Contains(typeAsInt))
+            {
+                tile = wallDiagonalCornerDownLeft;
+            }
+            else if (WallTypesHelper.wallDiagonalCornerDownRight.Contains(typeAsInt))
+            {
+                tile = wallDiagonalCornerDownRight;
+            }
+            else if (WallTypesHelper.wallDiagonalCornerUpRight.Contains(typeAsInt))
+            {
+                tile = wallDiagonalCornerUpRight;
+            }
+            else if (WallTypesHelper.wallDiagonalCornerUpLeft.Contains(typeAsInt))
+            {
+                tile = wallDiagonalCornerUpLeft;
+            }
+            else if (WallTypesHelper.wallFullEightDirections.Contains(typeAsInt))
+            {
+                tile = wallFull;
+            }
+            else if (WallTypesHelper.wallBottmEightDirections.Contains(typeAsInt))
+            {
+                tile = wallBottom;
+            }
+
+            if (tile != null)
+            {
+                PaintSingleTile(wallTilemap, tile, position);
+            }
+
         }
 
         private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position)
         {
-            var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+            Vector3Int tilePosition = tilemap.WorldToCell((Vector3Int)position);
             tilemap.SetTile(tilePosition, tile);
         }
 
@@ -45,10 +121,7 @@ namespace TheProphecy.Grid.DungeonGeneration
             wallTilemap.ClearAllTiles();
         }
 
-        internal void PaintSingleCornerWall(Vector2Int position, string neighboursBinaryType)
-        {
-            throw new NotImplementedException();
-        }
+
     }
 
 }
