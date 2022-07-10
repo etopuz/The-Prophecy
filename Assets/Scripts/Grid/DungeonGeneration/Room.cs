d7cb5999185da92cbe065c82082649ac740ea1b8
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-10 18:52:28 +0300

    add: room travelsal attributes

--- Assets/Scripts/Grid/DungeonGeneration/Room.cs
+++ Assets/Scripts/Grid/DungeonGeneration/Room.cs
@@ -4,31 +4,43 @@ using UnityEngine;
 
 namespace TheProphecy.Grid.DungeonGeneration
 {
+    [System.Serializable]
     public class Room
     {
         private BoundsInt _bounds;
-
         public RoomType roomType;
+
+        private HashSet<Room> _neighbours;
+
+        public bool isVisited = false;
+        public int length = 0;
+
+
         public BoundsInt Bounds { get => _bounds; }
-        private HashSet<Room> neihbours;
 
         public Room(BoundsInt bounds)
         {
             _bounds = bounds;
-            neihbours = new HashSet<Room>();
+            _neighbours = new HashSet<Room>();
         }
 
         public void AddNeihbour(Room room)
         {
             if(room != this)
             {
-                neihbours.Add(room);
+                _neighbours.Add(room);
             }
         }
 
         public HashSet<Room> GetNeighbourRooms()
         {
-            return neihbours;
+            return _neighbours;
+        }
+
+        public void ClearTravelStatus()
+        {
+            isVisited = false;
+            length = 0;
         }
     }
 }
