f376bdbe2c6091e4570b7d3e7450831dd70939e3
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-12 19:42:09 +0300

    add: "references for ai" script for parent object of generated mobs

---
+++ Assets/Scripts/Enemy/AccessReferencesForAI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class AccessReferencesForAI : Singleton<AccessReferencesForAI>
+{
+    public GameObject pathfindingGrid;
+
+    public GameObject targetLeftPivot;
+    public GameObject targetRightPivot;
+}
