73e7565828934515c133e72c98ca01a8b42a10a0
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-16 21:07:56 +0300

    add: IDamageable interface

---
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheProphecy.Interfaces
+{
+    public interface IDamageable
+    {
+        void OnTakeDamage(int Damage);
+    }
+}
+
