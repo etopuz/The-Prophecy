fd77da442fb98eda491d2fba44827266538c442b
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-16 21:08:22 +0300

    change: healthbar system added

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
-
+using TheProphecy.Interfaces;
+using UnityEngine.UI;
+using TheProphecy;
 
 namespace TheProphecy.Enemy
 {
-    public class BaseEnemy : MonoBehaviour
+    public class BaseEnemy : MonoBehaviour, IDamageable
     {
+        [Header("Health Variables")]
+        [SerializeField] private const int MAX_HEALTH = 5;
+        [SerializeField] private int _health = 5;
 
-        private int _health = 5;
+        [Header("HealthBar")]
+        [SerializeField] private Canvas _healthBar;
+        [SerializeField] private Image _healthBarSlider;
 
-        public void TakeDamage(int damage)
+        public void Start()
+        {
+            _health = MAX_HEALTH;
+        }
+
+        public void OnTakeDamage(int damage)
         {
             _health -= damage;
 
             if (_health < 1)
                 gameObject.SetActive(false);
+
+            UpdateHealthBar();
+        }
+
+        private void UpdateHealthBar()
+        {
+            if (_healthBar.enabled == false)
+            {
+                _healthBar.enabled = true;
+            }
+
+            _healthBarSlider.fillAmount = Mathf.Clamp01((float) (_health + 0.1f) / (float)MAX_HEALTH );
         }
     }
 }
