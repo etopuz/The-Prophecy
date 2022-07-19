daa8e28dc11b5a30586dee58ef511615ae540ffd
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-20 02:20:01 +0300

    Update: camera follows character more smoother

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
-    [SerializeField] private float _smoothSpeed = 0.1f;
+    [SerializeField] [Range(0.1f, 1f)] private float _smoothSpeed = 0.125f;
+
+    private Vector3 _offset;
+    private Vector3 _velocity;
+
+    private void Start()
+    {
+        _velocity = Vector3.zero;
+        _offset = Vector3.forward * (-10);
+        transform.position = _target.position + _offset;
+    }
+
 
     private void FixedUpdate()
     {
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, _target.position, _smoothSpeed);
-        smoothedPosition.z = transform.position.z;
-        transform.position = smoothedPosition;
+        Vector3 desiredPosition = _target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, _smoothSpeed);
     }
 }
