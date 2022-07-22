16b58c08a12ded2b95be87d0ca8dbf2ba71dc307
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-23 01:27:05 +0300

    rename: "UIController" class to "SkillButtonController" class

--- Assets/Scripts/UI/SkillButtonController.cs
+++ Assets/Scripts/UI/SkillButtonController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 namespace TheProphecy
 {
-    public class UIController : MonoBehaviour
+    public class SkillButtonController : MonoBehaviour
     {
 
         [Header("Controllers")]
