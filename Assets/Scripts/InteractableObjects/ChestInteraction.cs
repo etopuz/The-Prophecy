ec065922939d25dba717fa38d86134948592ddeb
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-07-29 05:20:24 +0300

    update: inventory system is removed for now

--- Assets/Scripts/InteractableObjects/ChestInteraction.cs
+++ Assets/Scripts/InteractableObjects/ChestInteraction.cs
@@ -10,13 +10,6 @@ public class ChestInteraction : MonoBehaviour, IInteractable
     [SerializeField] private SpriteRenderer _openedChest;
     [SerializeField] private SpriteRenderer _closedChest;
     [SerializeField] private BoxCollider2D _interactableBoxCollider;
-    [SerializeField] private InventoryManager _inventorySystem;
-
-    private void Start()
-    {
-        Transform player = GameObject.FindGameObjectWithTag(TagLayerData.PLAYER).transform;
-        _inventorySystem = player.GetComponent<InventoryManager>();
-    }
 
     public void OnInteract()
     {
@@ -28,15 +21,12 @@ public class ChestInteraction : MonoBehaviour, IInteractable
         LevelRunStats levelStats = LevelManager.instance.levelRunStats;
         bool canOpenChest = levelStats.TryToUseKey();
 
-        ItemDatabase itemDatabase = _inventorySystem.itemDatabase;
 
         if (canOpenChest)
         {
             _openedChest.enabled = true;
             _closedChest.enabled = false;
             _interactableBoxCollider.enabled = false;
-            ItemSO randomItem = itemDatabase.allItems.ReturnRandomElement();
-            _inventorySystem.TryToAddItem(randomItem);
         }
     }
 }
